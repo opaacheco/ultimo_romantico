b724946ecfd0a868bd412de93f957db411d93ebf
Pedro Gomes <aformigamorreupedro@gmail.com>
2023-07-28 22:54:57 +0100

    fim do jogo pra mim e pra le

--- script/backend.js
+++ script/backend.js
@@ -1,3 +1,4 @@
+var vida = 4; // armazena a quantidade de vidas perdida pelo utilziador
 var pontos = 0; // armazena a quantidade de pontos obtido pelo jogador
 var guardarElemento = null // guarda o primeiro elemento para posteriormente ser utilizado 
 var descricaoImagemUm = null; // alt da imagem para ser comparado no fim
@@ -21,12 +22,15 @@ function verificarIgualdade(element) {
         const descricaoImagemDois = element.alt; // Armazena o valor do atributo 'alt' da segunda imagem clicada
         virarCard(element);
         if (descricaoImagemUm === descricaoImagemDois) {
-            pontos = pontos + 1;
             element.setAttribute("data-matched", "true");
             guardarElemento.setAttribute("data-matched", "true");
             console.log("Par de imagens igual encontrado!");
+            pontos = pontos + 1;
+            mostrarVitoria();
         } else {
             console.log("Par de imagens diferente.");
+            vida = vida - 1;
+            mostrarErros();
             lockVirar = true;
             setTimeout(function(){
             virarCard(guardarElemento)
@@ -37,10 +41,32 @@ function verificarIgualdade(element) {
         descricaoImagemUm = null; // Reseta a variável para que a próxima rodada comece novamente
         }  
 }
+
 function virarCard(element) {
     if(element.style.opacity == "1"){
         element.style.opacity = "0"
     }else{
         element.style.opacity = "1"
+    }
+}
+
+function mostrarErros() {
+    document.getElementById('vidaCounter').innerText = vida
+    if(vida == 0){
+        console.log("teste")
+        document.getElementById("pop-up-derrota").style.display = "block"
+        document.getElementById("conteudo-completo").style.display = "none"
+    }
+}
+
+function fecharPopUp(){
+    location.reload()
+}
+
+function mostrarVitoria() {
+    if(pontos == 4){
+        console.log("você ganhou")
+        document.getElementById("pop-up-vitoria").style.display = "block"
+        document.getElementById("conteudo-completo").style.display = "none"
     }
 }
